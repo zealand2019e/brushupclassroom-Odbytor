ac3763208a3511795d25b5aa7a9afed6cd7d5787
Bartosz <DonMarchewa@gmail.com>
2019-09-04 12:28:59 +0200

    Task 5

--- ClassRoom/ClassRoom.cs
+++ ClassRoom/ClassRoom.cs
@@ -8,9 +8,9 @@ namespace ClassRoom
 {
     class ClassRoom
     {
-       public string ClassName;
-       public List<string> Classlist;
-       public DateTime SemesterStart;
+        public string ClassName;
+        public List<string> Classlist;
+        public DateTime SemesterStart;
 
 
         public ClassRoom()
@@ -22,5 +22,20 @@ namespace ClassRoom
         {
             return $"Class Name: {ClassName}, Class list: {Classlist.ToString()}, Semester starts: {SemesterStart}";
         }
+
+        public string season(DateTime date)
+        {
+            if (date.Month == 12 || date.Month == 1 || date.Month == 2)
+                return "Winter";
+            else if (date.Month == 6 || date.Month == 7 || date.Month == 8)
+                return "Summer";
+            else if (date.Month == 9 || date.Month == 11 || date.Month == 10)
+                return "Autumn";
+            else if (date.Month == 3 || date.Month == 4 || date.Month == 5)
+                return "Spring";
+            
+                return "No";
+        }
+
     }
 }
